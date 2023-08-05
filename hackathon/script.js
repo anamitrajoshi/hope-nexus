8622b85cf89761d9b2ff44fa0690c86fb0494264
anamitrajoshi <anamitra.joshi@gmail.com>
2023-08-05 22:10:58 +0530

    file added

---
+++ hackathon/script.js
@@ -0,0 +1,27 @@
+const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+const loginLink = document.getElementById('login-link');
+const signupLink = document.getElementById('signup-link');
+
+loginLink.addEventListener('click', () => {
+    loginForm.classList.add('active');
+    signupForm.classList.remove('active');
+});
+
+signupLink.addEventListener('click', () => {
+    signupForm.classList.add('active');
+    loginForm.classList.remove('active');
+});
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // Store data in LocalStorage
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+    
+    console.log('Data stored successfully.');
+});
---
+++ hackathon/script1.js
@@ -0,0 +1,16 @@
+function searchPeople() {
+    const searchInput = document.getElementById('search-input').value.toLowerCase();
+    const people = document.querySelectorAll('.person');
+
+    // Clear previous search results
+    document.getElementById('search-results').innerHTML = '';
+
+    for (const person of people) {
+        const skills = person.querySelector('p').innerText.toLowerCase();
+        if (skills.includes(searchInput)) {
+            // Clone the matched person and append to the search results
+            const clonedPerson = person.cloneNode(true);
+            document.getElementById('search-results').appendChild(clonedPerson);
+        }
+    }
+}
